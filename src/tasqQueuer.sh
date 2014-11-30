65cd65c0b348f8e54055dba018c61b6707bac789
Alan Ghobadi <aalan3@gmail.com>
2014-12-01 00:04:13 +0900

    Updated bash script to be more autonomous
    Data files are generated and folder structure created

--- src/tasqQueuer.sh
+++ src/tasqQueuer.sh
@@ -2,14 +2,41 @@
 DES_EXEC_STD=./des_c
 DES_EXEC_CUDA=./des_cu
 KEY_FILE=key
-DATA_FILES=../../experiments/data/*
-OUTPUT_FOLDER=../../experiments/output
-TASQ_OUTPUT_FOLDER=../../experiments/tasq_output
+DATA_FOLDER=data
+OUTPUT_FOLDER=output
+TASQ_OUTPUT_FOLDER=tasq_output
 RUNS=1
 THREADS_PER_BLOCK=500
-MAX_FILE_SIZE=$((10**7))
+MAX_FILE_SIZE=$((10**4))
 
-for DATAFILE in $DATA_FILES; do
+# Create required folder structures
+rm -r ${OUTPUT_FOLDER}
+rm -r ${TASQ_OUTPUT_FOLDER}
+rm -r ${DATA_FOLDER}
+mkdir -p ${OUTPUT_FOLDER}/{std,cuda}
+mkdir -p ${TASQ_OUTPUT_FOLDER}/{std,cuda}
+mkdir ${DATA_FOLDER}
+
+# Create keyfile
+dd if=/dev/urandom of=key bs=8 count=1
+
+# Create data
+file_size=$((2**10))
+while [ $file_size -lt $MAX_FILE_SIZE ]; do
+  echo $file_size
+  if [ $file_size -lt $((2**20)) ]; then
+    file_name="$(($file_size/1024))K"
+  elif [ $file_size -lt $((2**30)) ]; then
+    file_name="$(($file_size/2**20))M"
+  else
+    file_name="$(($file_size/2**30))G"
+  fi
+  dd if=/dev/urandom of=$DATA_FOLDER/$file_name bs=$file_size count=1
+  file_size=$(($file_size*2))
+done
+
+
+for DATAFILE in ${DATA_FOLDER}/*; do
 	FILESIZE=$(stat -c%s "$DATAFILE")
 	if [[ "$FILESIZE" -gt "$MAX_FILE_SIZE" ]]; then
 		echo Ignoring $DATAFILE with $FILESIZE bytes
