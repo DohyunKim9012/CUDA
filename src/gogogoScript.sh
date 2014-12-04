ffa9e9c8c309fa09162f852d5d42b2846685f671
Alan Ghobadi <aalan3@gmail.com>
2014-12-04 18:18:05 +0900

    gogogoScript.sh

---
+++ src/gogogoScript.sh
@@ -0,0 +1,17 @@
+ tasq_output/cuda results/GO_512_tasq_output
+ ./tasqQueuer.sh -c -t 
+ ./resultReader.sh > results/GO_512_threads
+ cp -R tasq_output/cuda results/GO_512_tasq_output
+ ./tasqQueuer.sh -t 512
+ ./resultReader.sh > results/GO_512_threads
+ cp -R tasq_output/cuda results/GO_512_tasq_output
+ ./tasqQueuer.sh -t 512
+ ./resultReader.sh > results/GO_512_threads
+ cp -R tasq_output/cuda results/GO_512_tasq_output
+ ./tasqQueuer.sh -t 512
+ ./resultReader.sh > results/GO_512_threads
+ cp -R tasq_output/cuda results/GO_512_tasq_output
+ ./tasqQueuer.sh -t 512
+ ./resultReader.sh > results/GO_512_threads
+ cp -R tasq_output/cuda results/GO_512_tasq_output
+
